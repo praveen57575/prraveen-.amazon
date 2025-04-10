7144aa3f51ab73735106b58fed06d6da1d7c52a3
praveen <praveeinfo7@gmail.com>
2025-04-10 16:54:40 +0530

    pravee

---
+++ data.js
@@ -0,0 +1,40 @@
+//datatypes
+
+//primitive
+
+//string ""
+//number 123 || 12.5
+//boolean true or false
+//null
+//undefined
+
+//string
+console.log("pravee");
+
+//num
+console.log(12.5);
+
+//boolean
+console.log(true);
+
+//null
+console.log(null);
+
+//undefined
+var a;
+console.log(a);
+
+//non-primitive
+
+//object {}
+var ob ={
+    name : "pravee",
+    id : 57,
+    num : 575,
+    job : "it"
+}
+console.log(ob);
+
+//array[]
+let sp = [12.5,"spk",57, null, undefined]
+console.log(sp);
